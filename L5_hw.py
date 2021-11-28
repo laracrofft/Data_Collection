a9e5a5b6c52cc22a7522f9b29dec25c63994d2be
laracrofft <lara-on@yandex.ru>
2021-11-28 22:30:49 +0300

    hw added

---
+++ L5_hw.py
@@ -0,0 +1,61 @@
+'''
+Написать программу, которая собирает входящие письма из своего или тестового почтового ящика
+и сложить данные о письмах в базу данных (от кого, дата отправки, тема письма, текст письма полный)
+Логин тестового ящика: study.ai_172@mail.ru
+Пароль тестового ящика: NextPassword172#
+'''
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from pymongo import MongoClient
+from pprint import pprint
+
+driver = webdriver.Chrome(executable_path=r'c:/Users/larch/chromedriver')
+
+driver.get('https://mail.ru/')
+
+elem = driver.find_element(By.XPATH, '//input[@name="login"]')
+elem.send_keys('study.ai_172')
+elem.send_keys(Keys.ENTER)
+
+driver.implicitly_wait(1.5)
+
+elem = driver.find_element(By.XPATH, '//input[@name="password"]')
+elem.send_keys('NextPassword172#')
+elem.send_keys(Keys.ENTER)
+
+driver.implicitly_wait(10)
+
+email_links = {}
+new_links = driver.find_elements(By.XPATH, "//a[contains(@class, 'js-letter-list-item')]")
+
+for l in new_links:
+        email_url = l.get_attribute('href')
+        email_links[email_url] = l
+
+# pprint(email_links)
+
+client = MongoClient('localhost', 27017)
+mongo_db = client['mailru']
+inbox_letters = mongo_db.inbox_letters
+
+for link in email_links.items():
+    driver.get(link[0])
+    letter_total = {}
+    email_from = driver.find_element(By.XPATH, "//div[@class='letter__author']/span[@class='letter-contact']").text
+    email_date = driver.find_element(By.XPATH, "//div[@class='letter__date']").text
+    subject = driver.find_element(By.XPATH, "//h2[@class='thread__subject']").text
+    letter = driver.find_element(By.XPATH, "//div[@class='letter__body']").text
+
+    letter_total['from_'] = email_from
+    letter_total['date'] = email_date
+    letter_total['subject'] = subject
+    letter_total['letter'] = letter
+
+    inbox_letters.insert_one(letter_total)
+
+
+
+
